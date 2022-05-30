ec02d87711c3d3b06767111cbec0d4e87f72578c
RaulRagel <raul.rs3006@gmail.com>
2022-05-30 21:25:21 +0200

    subimos los proyectos

---
+++ filtered-list/filtered-list/filter.js
@@ -0,0 +1,49 @@
+
+var listElement;
+var defaultItems;
+
+window.onload = function(){
+
+    //get list
+    listElement = document.querySelector("#filtered-list");
+
+    //get default item list
+    var nodeList = listElement.querySelectorAll("li");
+
+    defaultItems = Array.from(nodeList).map(
+        item => item.innerText
+    );
+
+    //add listener to the filter and link them to his lists
+    document.querySelector("#filter").addEventListener('input', function(){
+        
+        var filter = document.querySelector("#filter").value;
+        filterList(filter);
+    }, false);
+}
+
+function filterList(filter){
+    
+    var filteredItems = defaultItems.filter(item => item.toUpperCase().includes(filter.toUpperCase()));
+    
+    //update list element with the filtered list
+    updateList(filteredItems);
+}
+
+function updateList(filteredItems){
+
+    listElement.innerHTML = '';
+
+    filteredItems.forEach(function(item){
+
+        listElement.appendChild(createListElement(item));
+    })
+}
+
+function createListElement(item){
+
+    var li = document.createElement("li");
+    li.innerText = item;
+
+    return li;
+}
